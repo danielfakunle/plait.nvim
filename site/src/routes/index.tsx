88941d7ee231ecaa35bc249eb5843cb21d6f4b1a
Daniel Fakunle <tosinfakunle@yahoo.com>
2026-09-07 23:11:31 -0700

    chore(site): format

--- site/src/routes/index.tsx
+++ site/src/routes/index.tsx
@@ -8,9 +8,7 @@ function Home() {
   return (
     <main className="container container-padding-x mx-auto py-24">
       <h1 className="mt-3 heading-md">Title.</h1>
-      <p className="mt-4 text-base max-w-2xl text-muted-foreground">
-        Subtitle
-      </p>
+      <p className="mt-4 text-base max-w-2xl text-muted-foreground">Subtitle</p>
       <Link
         className={cn(buttonVariants({ variant: "link", size: "lg", className: "px-0 mt-4" }))}
         to="/docs/$"
